0b175496d253eb0772ac95dba9ad05c1e31da2a2
Rodrigo Baptista <digodrigodev@gmail.com>
2024-09-01 10:34:39 -0300

    primeiro commit

---
+++ src/main/java/com/example/mentorias/globally/MentoriasGloballyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentorias.globally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoriasGloballyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoriasGloballyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mentorias/globally/MentoriasGloballyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentorias.globally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriasGloballyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
